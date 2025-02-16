fa38cb8af08baf28931b977e0d2a0a606e17acaa
sudheerpailu <sudheersrinivasa7@gmail.com>
2025-02-16 17:36:45 +0530

    initial commit

---
+++ helloworld/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const name, age = "sudheer", 23
+	fmt.Printf("%s is of age %d\n", name, age)
+	// fmt.Println("Hello world")
+}
---
+++ variables.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var speed = 100
+	fmt.Printf("The speed of car is %d and the speed kills\n", speed)
+}
